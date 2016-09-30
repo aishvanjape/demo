90a208d2fbc7d75703e6d3109e1a7926a422552b
aishvanjape <training@DIN51001807.corp.capgemini.com>
2016-09-30 16:55:12 +0530

    EMS

--- eclipse-workspcae/demo-jdbc-person/src/Person.java
+++ eclipse-workspcae/demo-jdbc-person/src/Person.java
@@ -1,19 +1,22 @@
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Properties;
 
 public class Person {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, SQLException {
 		
 		Properties props = new Properties();
 		
-		FileInputStream fln = new FileInputStream("dbDetails.properties");
+		FileInputStream fln = new FileInputStream("dbdetails.properties");
 		props.load(fln);
 		
+		String url = props.getProperty("url");
+		Connection dbconnection;
 		
-		
-		
-		
+		dbconnection = DriverManager.getConnection(url);
 	}
 }
