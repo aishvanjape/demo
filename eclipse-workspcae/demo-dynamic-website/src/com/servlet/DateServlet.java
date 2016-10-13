d0cf03d75d2b0c4743b7ec4e7a909077ae637952
aishvanjape <training@DIN51001807.corp.capgemini.com>
2016-10-13 16:59:11 +0530

    13/10/16

--- eclipse-workspcae/demo-dynamic-website/src/com/servlet/DateServlet.java
+++ eclipse-workspcae/demo-dynamic-website/src/com/servlet/DateServlet.java
@@ -2,6 +2,7 @@ package com.servlet;
 
 import java.io.IOException;
 import java.util.Date;
+import java.util.Locale;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
@@ -10,6 +11,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
+import org.apache.tomcat.jni.Local;
 
 @WebServlet("/getDate")
 public class DateServlet extends HttpServlet{
@@ -26,7 +29,7 @@ public class DateServlet extends HttpServlet{
 		
 //		HttpSession session = req.getSession();
 //		System.out.println(session.getId());
-		
+
 		System.out.println(getServletContext());
 		System.out.println(getServletConfig());
 		(resp.getWriter()).println(new Date());
--- eclipse-workspcae/demo-dynamic-website/src/com/servlet/SessionServlet.java
+++ eclipse-workspcae/demo-dynamic-website/src/com/servlet/SessionServlet.java
@@ -2,6 +2,7 @@ package com.servlet;
 
 import java.io.IOException;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -17,11 +18,12 @@ public class SessionServlet extends HttpServlet{
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		HttpSession session = req.getSession();
 		//System.out.println(session.getId());
-		
+		ServletContext
 		String encodeurl = resp.encodeURL("getDate");
 		
 		resp.sendRedirect(encodeurl);
 		
+		//req.getRequestDispatcher("getDate").include(req, resp);
 		
 	}
 	
